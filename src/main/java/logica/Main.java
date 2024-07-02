5754030b9a886033b1d8fb58d19087684a5c3285
Heiler <pheilerggomez@gmail.com>
2024-07-02 18:10:08 -0400

    intento 2

---
+++ src/main/java/logica/Main.java
@@ -0,0 +1,18 @@
+package logica;
+
+import gui.Login;
+
+public class Main {
+    
+    
+    
+    public static void main(String[] args) {
+        
+         Login log = new Login();
+        log.setVisible(true);
+       
+    }
+
+
+    
+}
